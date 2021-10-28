2774bed8b6da9fb728a713095763465f04016a2d
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-10-28 16:10:19 +0100

    dns: Add DNS records for mail

--- cloudflare/vivaanverma.tf
+++ cloudflare/vivaanverma.tf
@@ -1,9 +1,9 @@
 terraform {
     required_providers {
-      cloudflare = {
-          source = "cloudflare/cloudflare"
-          version = "~> 3.3.0"
-      }
+        cloudflare = {
+            source  = "cloudflare/cloudflare"
+            version = "~> 3.3.0"
+        }
     }
 }
 
@@ -11,6 +11,7 @@ provider "cloudflare" {
     # Email and API Token read from environment variables
 }
 
+# Details found on the Cloudflare Dashboard - "DNS" tab
 variable "zone_id" {
     default = "05d8538fd89486d425bd9e2e25d8bf79"
 }
@@ -19,11 +20,68 @@ variable "domain" {
     default = "vivaanverma.com"
 }
 
-resource "cloudflare_record" "www" {
-    zone_id = var.zone_id
-    name    = "www"
-    value   = var.domain
-    type    = "CNAME"
-    proxied = true
+# DNS Records
+resource "cloudflare_record" "www-redirect" {
+    zone_id         = var.zone_id
+    name            = "www"
+    value           = var.domain
+    type            = "CNAME"
+    proxied         = true
+    allow_overwrite = true
+}
+
+resource "cloudflare_record" "site-record" {
+    zone_id         = var.zone_id
+    name            = "@"
+    value           = "76.76.21.21"
+    type            = "A"
+    proxied         = true
+    allow_overwrite = true
+}
+
+resource "cloudflare_record" "git-shortcut" {
+    zone_id         = var.zone_id
+    name            = "git"
+    value           = "100::"
+    type            = "AAAA"
+    proxied         = true
+    allow_overwrite = true
+}
+
+resource "cloudflare_record" "mail-redirect-1" {
+    zone_id         = var.zone_id
+    name            = "@"
+    value           = "mx1.improvmx.com"
+    type            = "MX"
+    proxied         = false
+    priority        = 10
+    allow_overwrite = true
+}
+
+resource "cloudflare_record" "mail-redirect-2" {
+    zone_id         = var.zone_id
+    name            = "@"
+    value           = "mx2.improvmx.com"
+    type            = "MX"
+    proxied         = false
+    priority        = 20
+    allow_overwrite = true
+}
+
+resource "cloudflare_record" "spf-record" {
+    zone_id         = var.zone_id
+    name            = "@"
+    value           = "v=spf1 inlcude:spf.improvmx.com ~all"
+    type            = "TXT"
+    proxied         = false
+    allow_overwrite = true
+}
+
+resource "cloudflare_record" "dmarc-record" {
+    zone_id         = var.zone_id
+    name            = "_dmarc"
+    value           = "v=DMARC; p=reject; rua-mailto:vivaan.verma@gmail.com"
+    type            = "TXT"
+    proxied         = false
     allow_overwrite = true
 }

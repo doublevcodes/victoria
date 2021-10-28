6efcc6a08c0130e94e0e18f4be4e44a254e61a3d
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-10-28 14:18:39 +0100

    deps: Bump provider and remove zone settings

--- cloudflare/vivaanverma.tf
+++ cloudflare/vivaanverma.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
       cloudflare = {
           source = "cloudflare/cloudflare"
-          version = "~> 3.2.0"
+          version = "~> 3.3.0"
       }
     }
 }
@@ -26,19 +26,4 @@ resource "cloudflare_record" "www" {
     type    = "CNAME"
     proxied = true
     allow_overwrite = true
-}
-
-resource "cloudflare_zone_settings_override" "vivaanverma-com-settings" {
-    zone_id = var.zone_id
-    settings {
-        always_online = "on"
-        always_use_https = "on"
-        brotli = "on"
-        hotlink_protection = "on"
-        http3 = "on"
-        rocket_loader = "on"
-        min_tls_version = "1.3"
-        ssl = "strict"
-        tls_1_3 = "on"
-    }
 }

0309d8fc3443aceca316c2de9b10e3abb184f9a7
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-11-21 19:16:51 +0000

    tf fmt

--- modules/page_rules/main.tf
+++ modules/page_rules/main.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-      cloudflare {
-        source = "cloudflare/cloudflare"
-        version = "~> 3.3"
-      }
+  required_providers {
+    cloudflare {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.3"
     }
+  }
 }
 
 variable "zone_id" {
@@ -17,10 +17,10 @@ variable "domain" {
 
 resource "cloudflare_page_rule" "git-subdomain" {
   zone_id = var.zone_id
-  target = "git.${var.domain}/*"
+  target  = "git.${var.domain}/*"
   actions {
     forwarding_url {
-      url = "https://github.com/doublevcodes/$1"
+      url         = "https://github.com/doublevcodes/$1"
       status_code = 301
     }
   }

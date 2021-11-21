64e070c1fe26321e3c4551d34f88d921fddf0c82
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-11-21 20:24:27 +0000

    feat: create access policy for ssh in browser

--- modules/access_apps/main.tf
+++ modules/access_apps/main.tf
@@ -21,3 +21,19 @@ resource "cloudflare_access_application" "ssh_in_browser" {
   domain  = "ssh.${var.domain}"
   type    = "ssh"
 }
+
+resource "cloudflare_access_policy" "ssh_in_browser_policy" {
+  application_id = cloudflare_access_application.ssh_in_browser.zone_id
+  zone_id = var.zone_id
+  name    = "SSH in Browser Policy"
+  precedence = "1"
+  decision = "allow"
+
+  include {
+    email = ["vivaan.verma@gmail.com"]
+  }
+
+  require {
+    email = ["vivaan.verma@gmail.com"]
+  }
+}
